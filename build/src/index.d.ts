80d49a13701784341e72f75f71f8351810d7afd8
LaukikOnMac <22937426+LRagji@users.noreply.github.com>
2020-10-08 18:03:56 +0530

    Removed unwanted files

--- build/src/index.d.ts
+++
@@ -1,8 +0,0 @@
-import * as pg from 'pg-promise';
-export declare class PgBoot {
-    version: number;
-    name: string;
-    constructor(name: string);
-    static dynamicPreparedStatement(name: string, sql: string, dynamicValues: any): pg.PreparedStatement;
-    checkVersion(pgWriterConnection: pg.IDatabase<any>, expectedSchemaVersion: number, reconcileCallback: (transaction: pg.ITask<any>, dbVersion: number) => Promise<void>): Promise<boolean>;
-}
