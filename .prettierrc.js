21e23887a3937e2142f31ea87f070ee490d7d910
LaukikOnMac <22937426+LRagji@users.noreply.github.com>
2020-10-08 18:02:07 +0530

    Ground Zero

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+{
+  "bracketSpacing": true,
+  "endOfLine": "auto",
+  "htmlWhitespaceSensitivity": "css",
+  "printWidth": 80,
+  "semi": true,
+  "singleQuote": true,
+  "tabWidth": 2,
+  "useTabs": false,
+  "trailingComma": "all"
+}
---
+++ examples/index.js
@@ -0,0 +1,26 @@
+const bootType = require('../build/src/index');
+const pgp = require('pg-promise')({schema: 'Boot'});
+const defaultConectionString = 'postgres://postgres:@localhost:5432/QUEUE';
+const writeConfigParams = {
+  connectionString: defaultConectionString,
+  application_name: 'Example1-Queue-Writer',
+  max: 2, //2 Writer
+};
+const pgWriter = pgp(writeConfigParams);
+const instance = new bootType.PgBoot('ProductName');
+
+async function UpgradeHandler(transaction, dbVersion) {
+  switch (dbVersion) {
+    case -1: //Green field(Nothing is present in DB) first time install
+      await transaction.none('CREATE TABLE "V1" ();');
+      break;
+    case 0: //Version zero was already present
+      await transaction.none('ALTER TABLE V1 RENAME "V2";');
+      break;
+  }
+}
+
+instance
+  .checkVersion(pgWriter, 0, UpgradeHandler)
+  .then(console.log)
+  .catch(console.error);
